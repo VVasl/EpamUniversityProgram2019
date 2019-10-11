8379dcad97d97e4fc41d046e30f68526fa744ef9
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-10-12 00:00:49 +0300

    add exception snippets

---
+++ Exceptions/Exceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exceptions
+{
+    public class ExceptionSituations
+    {
+        public static void Recurse(int remaining)
+        {
+            if (remaining <= 0)
+            {
+                return;
+            }
+            Recurse(remaining - 1);
+        }
+
+        public static void WriteArray(int n)
+        {
+            int[] array = new int[n];
+
+            for (int i = 0; i <= array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static int DoSomeMath(int a, int b)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Parameter should be greater than 0", "a");
+            }
+
+            if (b > 0)
+            {
+                throw new ArgumentException("Parameter should be less than 0", "b");
+            }
+
+            return a * b;
+        }
+    }
+}
