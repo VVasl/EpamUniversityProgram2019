47d0f7bed7b58c56fbca225e87fa2b402503b4a0
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-10-15 20:27:57 +0300

    add fileSystem tasks

---
+++ FileSystem/ContentOfTheDirectory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using Common;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FileSystem
+{
+    public class ContentOfTheDirectory : IValidateParameters
+    {
+        private const int MAX_RECURSIVE_CALLS = 1000;
+        static int counter = 0;
+        public string SourceDirectory { get; set; }
+
+        public void ShowAllDirectoryAndSubdirectoriesFiles(string sourceDirectory, IWriter writer)
+        {
+                string[] files = Directory.GetFiles(sourceDirectory);
+                foreach (string file in files)
+                {
+                    writer.Write($"\tFile: {file}");
+                }
+
+
+                string[] subDirectories = Directory.GetDirectories(sourceDirectory);
+
+                foreach (string subDirectory in subDirectories)
+                {
+                    writer.Write($"Subdirectory: {subDirectory}");
+
+                    counter++;
+                    if (counter <= MAX_RECURSIVE_CALLS)
+                        ShowAllDirectoryAndSubdirectoriesFiles(subDirectory, writer);
+                    else
+                        throw new StackOverflowException("Detected unhandled exception: Stack overflow.");
+                }
+            
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            
+                if (string.IsNullOrWhiteSpace(SourceDirectory) || SourceDirectory.Trim().Length == 0)
+                {
+                    isValid = false;
+                    throw new ArgumentException("String can't be null or empty.");
+                }
+            
+
+            return isValid;
+        }
+
+    }
+}
---
+++ FileSystem/TxtFileSearcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Common;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileSystem
+{
+    public class TxtFileSearcher : IValidateParameters
+    {
+        public string SourceDirectory { get; set; }
+        public string PartialNameOfFile { get; set; }
+        public void SearchTxtFiles( IWriter writer)
+        { 
+            try
+            {
+                var txtFiles = Directory.EnumerateFiles(SourceDirectory, "*.txt", SearchOption.AllDirectories);
+
+                foreach (string currentFile in txtFiles)
+                {
+                    if (currentFile.Contains(PartialNameOfFile, StringComparison.CurrentCultureIgnoreCase)){
+                        writer.Write(currentFile);
+                    }
+                }
+             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(SourceDirectory) || SourceDirectory.Trim().Length == 0)
+            {
+                isValid = false;
+                throw new ArgumentException("String can't be null or empty.");
+            }
+
+            if (string.IsNullOrWhiteSpace(PartialNameOfFile) || PartialNameOfFile.Trim().Length == 0)
+            {
+                isValid = false;
+                throw new ArgumentException("String can't be null or empty.");
+            }
+
+
+            return isValid;
+        }
+    }
+}
