f198701ae56bb3298592cc68632fa0c40bd92283
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-10-29 01:13:38 +0200

    console app for testing custom logger

---
+++ DumpLogger/Program.cs
@@ -0,0 +1,30 @@
+﻿using Exceptions;
+using Logger;
+
+namespace DumpLogger
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLogger logger = new MyLogger();
+            logger.Info("\n-----Task2-----IndexOutOfRangeException\n\n", Operation.LoggingToConsole);
+
+            int n = 7;
+
+            try
+            {
+                ExceptionSituations.InstantiateArray(n, 100);
+            }
+            catch (System.IndexOutOfRangeException e)
+            {
+                logger.Error(e.Message, Operation.LoggingToConsole);
+            }
+            catch (System.Exception e)
+            {
+                logger.Error(e.Message, Operation.LoggingToFile);
+                logger.ReadFromLogFile();
+            }
+        }
+    }
+}
