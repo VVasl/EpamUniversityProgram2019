63f8a7cd67bce57203c611c647b73d3763b47e04
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-10-31 22:42:29 +0200

    added  parameter validation service.

---
+++ Common/ParameterValidationService/ParameterValidation.cs
@@ -0,0 +1,77 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ParameterValidation.cs" company="Epam Lab">
+// Copyright (c) Epam Lab. All rights reserved.
+// </copyright>
+// <author>Vasyltsiv Viktoriia</author>
+//-----------------------------------------------------------------------
+namespace Common
+{
+    using System;
+
+    /// <summary>
+    /// Provides methods to validate argument parameters.
+    /// </summary>
+    public static class ParameterValidation
+    {
+            /// <summary>
+            /// Throw a <see cref="ArgumentNullException"/> if <paramref name="paramValue"/> is null.
+            /// </summary>
+            /// <typeparam name="T">The type of <paramref name="paramValue"/>.</typeparam>
+            /// <param name="paramValue">The value parameter.</param>
+            /// <param name="paramName">The name of the parameter.</param>
+            public static void AssertNotNull<T>(T paramValue, string paramName = null)
+            {
+                if (paramValue == null)
+                {
+                    throw new ArgumentNullException(paramName);
+                }
+            }
+
+            /// <summary>
+            /// Throw a <see cref="ArgumentException"/> if <paramref name="paramValue"/> is null or empty.
+            /// For Non-Null types, check if <paramref name="paramValue"/> equals the type default value.
+            /// For Null types, check if <paramref name="paramValue"/>.Value equals the type default value.
+            /// </summary>
+            /// <typeparam name="T">The type of <paramref name="paramValue"/>.</typeparam>
+            /// <param name="paramValue">The value parameter.</param>
+            /// <param name="paramName">The name of the parameter.</param>
+            public static void AssertNotNullOrEmpty<T>(T paramValue, string paramName = null)
+            {
+                    if (!paramValue.IsNullOrEmpty())
+                    {
+                        return;
+                    }
+                
+                    if (paramName == null)
+                    {
+                        throw new ArgumentException("The argument must not be empty.");
+                    }
+               
+                    throw new ArgumentException("The argument must not be empty.", paramName); 
+            }
+
+            /// <summary>
+            /// Throw a <see cref="ArgumentOutOfRangeException"/> if <paramref name="paramValue"/> is not greater than <paramref name="minValue"/>.
+            /// </summary>
+            /// <typeparam name="T">The type of <paramref name="paramValue"/>.</typeparam>
+            /// <param name="paramValue">The value parameter.</param>
+            /// <param name="minValue">The min value to compare to <paramref name="paramValue"/>.</param>
+            /// <param name="paramName">The name of the parameter.</param>
+            public static void AssertGreaterThan<T>(T paramValue, T minValue, string paramName = null)
+                where T : IComparable<T>
+            {
+                if (paramValue.IsGreaterThan(minValue))
+                {
+                    return;
+                }
+                
+                if (paramName == null)
+                {
+                    throw new ArgumentOutOfRangeException($"The argument is smaller than/equal to minimum of '{minValue}'");
+                }
+                   
+                throw new ArgumentOutOfRangeException(paramName,
+                               $"The argument '{paramName}' is smaller than/equal to minimum of '{minValue}'");
+            }
+    }
+}
---
+++ Common/ParameterValidationService/ValidationExtensions.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ValidationExtensions.cs" company="Epam Lab">
+// Copyright (c) Epam Lab. All rights reserved.
+// </copyright>
+// <author>Vasyltsiv Viktoriia</author>
+//-----------------------------------------------------------------------
+namespace Common
+{
+    using System;
+    using System.Collections;
+
+    public static class ValidationExtensions
+    {
+        /// <summary>
+        /// Check if the specified <paramref name="value"/> is null or empty.
+        /// </summary>
+        /// <typeparam name="T">The element type of the <paramref name="value"/>.</typeparam>
+        /// <param name="value">The value to check.</param>
+        /// <returns>true if the <paramref name="value"/> is null or empty; otherwise, false.</returns>
+        public static bool IsNullOrEmpty<T>(this T value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            var collection = value as IEnumerable;
+
+            foreach (var v in collection)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Check if the specified <paramref name="value"/> is null or empty.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns>true if the <paramref name="value"/> is null or empty; otherwise, false.</returns>
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+        
+        /// <summary>
+        /// Check if the specified <paramref name="value"/> is greater than <paramref name="min"/>.
+        /// </summary>
+        /// <typeparam name="T">The element type of the values to compare.</typeparam>
+        /// <param name="value">The value to check.</param>
+        /// <param name="min">The min value.</param>
+        /// <returns>true if the <paramref name="value"/> is greater than <paramref name="min"/>; otherwise, false.</returns>
+        public static bool IsGreaterThan<T>(this T value, T min) where T : IComparable<T>
+        {
+            ParameterValidation.AssertNotNull(value, nameof(value));
+            return value.CompareTo(min) > 0;
+        }
+    }
+}
