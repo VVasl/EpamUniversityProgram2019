348851b9f17743d2ea95b9db2080981fe2900f84
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-11-01 06:16:48 +0200

    fixed task according to the StyleCop rules

--- Exceptions/Exceptions.cs
+++ Exceptions/Exceptions.cs
@@ -1,19 +1,23 @@
-﻿using System;
-
-namespace Exceptions
+﻿namespace Exceptions
 {
+    using System;
+
     public static class ExceptionSituations
     {
         private const int MaxRecursiveCalls = 1000;
-        static int counter = 0;
+        private static int counter = 0;
 
         public static void Recurse()
         {
             counter++;
             if (counter <= MaxRecursiveCalls)
+            {
                 Recurse();
+            }
             else
+            {
                 throw new StackOverflowException("Detected exception: Stack overflow.");
+            }   
         }
 
         public static void InstantiateArray(int n, int maxValue)
