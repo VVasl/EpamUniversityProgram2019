3511f2cd7c7cdaf69c969eae0763c8411738073a
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-11-05 03:49:38 +0200

    refactored first excel task

---
+++ UniqueValuesInExcelFile/IValuesSearch.cs
@@ -0,0 +1,10 @@
+﻿namespace UniqueValuesInExcelFile
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IValuesSearch
+    {
+        HashSet<string> GetUniqueValuesFromLists(Tuple<HashSet<string>, HashSet<string>> countrylists);
+    }
+}
--- UniqueValuesInExcelFile/UniqueValuesExcel.cs
+++ UniqueValuesInExcelFile/UniqueValuesExcel.cs
@@ -2,32 +2,36 @@
 {   
     using System;
     using System.Collections.Generic;
-    using System.IO;
-    using OfficeOpenXml;
+    using Common;
 
-    public class UniqueValuesExcel : IExcel
+    public class UniqueValuesExcel : IValuesSearch
     {
-        public  Tuple<HashSet<string>, HashSet<string>> ReadListsFromFile(string excelFile, string sheetForReading, int firstColumnToCompare, int secondColumnToCompare)
+        private string excelFile;
+        public UniqueValuesExcel(string excelFile)
         {
-            var countryListFirst = new HashSet<string>();
-            var countryListSecond = new HashSet<string>();
+            this.ExcelFile = excelFile;
+        }
 
-            using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFile)))
+        public string ExcelFile
+        {
+            get
             {
-                // Get the first worksheet in the workbook.
-                ExcelWorksheet worksheet = package.Workbook.Worksheets[sheetForReading];
-
-                // Get row count.
-                int rowCount = worksheet.Dimension.End.Row;     
-                              
-                for (int row = 1; row <= rowCount; row++)
-                {
-                    countryListFirst.Add(worksheet.Cells[row, firstColumnToCompare].Value.ToString());
-                    countryListSecond.Add(worksheet.Cells[row, secondColumnToCompare].Value.ToString());
-                }
-
-                countryListFirst.UnionWith(countryListSecond);   
+                return this.excelFile;
             }
+
+            set
+            {
+                ParameterValidation.AssertNotNullOrEmpty(value, nameof(value));
+                this.excelFile = value;
+            }
+        }
+
+        public Tuple<HashSet<string>, HashSet<string>> GetLists(string sheetForReading, int firstColumnToCompare, int secondColumnToCompare)
+        {
+            var countryListFirst = new HashSet<string>(ExcelFileInputOutput.ReadListFromFile(this.ExcelFile, sheetForReading, firstColumnToCompare));
+            var countryListSecond = new HashSet<string>(ExcelFileInputOutput.ReadListFromFile(this.ExcelFile, sheetForReading, secondColumnToCompare));
+
+                countryListFirst.UnionWith(countryListSecond); 
 
             return Tuple.Create(countryListFirst, countryListSecond);
         }
@@ -40,23 +44,6 @@
             countryListFirst.UnionWith(countryListSecond);
 
             return countryListFirst;
-        }
-
-        public void WriteToExcel(string excelFile, HashSet<string> list, string sheetForWriting)
-        {
-            using (var package = new ExcelPackage(new FileInfo(excelFile)))
-            { 
-                var worksheet = package.Workbook.Worksheets[sheetForWriting];
-
-                int row = 1;
-                foreach (var item in list)
-                {
-                    worksheet.Cells[row, 1].Value = item;
-                    row++;
-                }
-
-                package.Save();
-            }
         }
     }
 }
