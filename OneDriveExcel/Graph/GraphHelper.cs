10ea9f6c59d190418afb80cd96c71d317e3eed59
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-12-08 23:58:43 +0200

    get user details and excel file

---
+++ OneDriveExcel/Graph/GraphHelper.cs
@@ -0,0 +1,46 @@
+﻿namespace OneDriveExcel   
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.Graph;
+
+    public class GraphHelper
+    {
+        private static GraphServiceClient graphClient;
+        public static void Initialize(IAuthenticationProvider authProvider)
+        {
+            graphClient = new GraphServiceClient(authProvider);
+        }
+
+        public static async Task<User> GetMeAsync()
+        {
+            try
+            {
+                return await graphClient.Me.Request().GetAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<Stream> GetFileAsync(string fileName)
+        {
+            try
+            {
+                var resultPage = await graphClient.Me.Drive.Root.ItemWithPath(fileName).Content
+                .Request()
+                .GetAsync();
+
+                return resultPage;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting file content: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
